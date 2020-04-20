ea173aba41ae4e1a7b2e8d98460ce4111c6ee56a
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-20 15:18:34 +0530

    Added: Employee Is Present Or Absent

---
+++ Employee/EmployeeWage.js
@@ -0,0 +1,8 @@
+let isPresent=1
+let employeeCheck=Math.floor(Math.random()*10)%2
+
+if (employeeCheck == isPresent){
+    console.log("Employee Is Present..")
+}else{
+    console.log("Employee Is Absent..")
+}
