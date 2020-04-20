0ef78abd0e4cef217365868b8186d917a99dd841
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-20 15:29:17 +0530

    Added: Employee Is Part Time or Full Time Or Absent

--- Employee/EmployeeWage.js
+++ Employee/EmployeeWage.js
@@ -1,8 +1,17 @@
-let isPresent=1
-let employeeCheck=Math.floor(Math.random()*10)%2
+let IS_PART_TIME=1
+let IS_FULL_TIME=2
+let EMPLOYEE_RATE_PER_HOUR=20
 
-if (employeeCheck == isPresent){
-    console.log("Employee Is Present..")
-}else{
-    console.log("Employee Is Absent..")
+let employeeHrs=0
+let empWage=0
+
+let employeeCheck=Math.floor(Math.random()*10)%3
+
+if (employeeCheck == IS_FULL_TIME){
+    employeeHrs = 8
+}else if(employeeCheck == IS_PART_TIME) {
+    employeeHrs = 4
+} else {
+    employeeHrs = 0
 }
+empWage=employeeHrs*EMPLOYEE_RATE_PER_HOUR;
